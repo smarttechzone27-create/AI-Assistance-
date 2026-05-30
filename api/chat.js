d70097784d5ea3f19c2cda1732aff2be4e152ef4
smarttechzone27-create <smarttechzone27@gmail.com>
2026-05-30 09:24:20 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   // Handle OPTIONS
-  if (req.method === "OPTIONS") {
+  f (req.method === "OPTIONS") {
     return res.status(200).end();
   }
 
